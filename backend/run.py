49ddb4ed2e87dcf201307c892e9bb58e2896c541
Hameedbalusupati <hameedbalusuapti023@gmail.com>
2026-03-23 15:30:21 +0530

    Initial commit

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,13 @@
+# backend/run.py
+
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # ✅ IMPORTANT
+    app.run(host="0.0.0.0", port=port)
